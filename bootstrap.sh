ca76565e8c633c8bb478f0a327af2ed7f3ea07eb
Alan Dipert <alan@dipert.org>
2016-12-21 20:10:11 -0800

    init

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y build-essential git openjdk-7-jdk maven wget
+
+wget -O /usr/local/bin/lein https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
+chmod a+x /usr/local/bin/lein
