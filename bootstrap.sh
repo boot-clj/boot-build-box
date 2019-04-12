8001895ee4f6f9a8c3d74d3b2b0c73115f9f738f
Alan Dipert <alan@dipert.org>
2019-04-12 12:53:04 -0700

    java8

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y build-essential git openjdk-7-jdk maven wget
+apt-get install -y build-essential git openjdk-8-jdk maven wget
 
 wget -O /usr/local/bin/lein https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
 chmod a+x /usr/local/bin/lein
